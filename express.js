3b7e54eeaeffc672a1760c525136987a5f7dced4
Alberto Guzman <112503817+TimmyElTaco@users.noreply.github.com>
2024-02-13 18:05:51 -0600

    Modify origin in cors

--- express.js
+++ express.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(cors({
-    origin: 'https://pomodoro-timer-gamma-lemon.vercel.app/'
+    origin: 'https://pomodoro-timer-gamma-lemon.vercel.app'
 }));
 
 app.get('/', (res) => {
